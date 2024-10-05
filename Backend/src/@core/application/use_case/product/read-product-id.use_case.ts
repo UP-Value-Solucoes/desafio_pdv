43a58f49bee840539970ed8367ac1e8a8c85ef69
JoseWenned <wennedchaves1997@gmail.com>
2024-10-05 14:43:46 -0300

    feat: create usecase read by id product

---
+++ Backend/src/@core/application/use_case/product/read-product-id.use_case.ts
@@ -0,0 +1,15 @@
+import { ProductRepositoryInterface } from "../../../domain/repositories/product/product.repository";
+import { TReadProductById } from "../../types/product/product.repository";
+
+export class ReadProductByIdUseCase {
+    constructor(private productRepo: ProductRepositoryInterface){};
+
+    async execute(productId: string): Promise<TReadProductById | null>{
+        const product: TReadProductById | null = await this.productRepo.findById(productId);
+        if(!product){
+            throw new Error("Product not found.")
+        }
+        return product;
+    };
+};
+
