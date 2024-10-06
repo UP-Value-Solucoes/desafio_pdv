b8278e996a8b710364c24e56167cf4a880479e5b
JoseWenned <wennedchaves1997@gmail.com>
2024-10-05 23:56:44 -0300

    feat: create routes product (post, get, getByid)

--- Backend/src/@core/application/use_case/product/read-list-products.use_case.ts
+++ Backend/src/@core/application/use_case/product/read-list-products.use_case.ts
@@ -1,7 +1,11 @@
-import { ProductRepositoryInterface } from "../../../domain/repositories/product/product.repository";
+import ProductRepositoryInterface from "../../../domain/repositories/product/product.repository";
 import { TReadListProduct } from "../../types/product/product.repository";
 
 export class ReadListProductUseCase {
+    // getListProduct: any;
+    // // getListProduct(): void | Promise<void> {
+    // //     throw new Error("Method not implemented.");
+    // // }
     constructor(private productRepo: ProductRepositoryInterface){};
 
     async execute(): Promise<TReadListProduct>{
--- Backend/src/@core/domain/repositories/product/product.repository.ts
+++ Backend/src/@core/domain/repositories/product/product.repository.ts
@@ -1,8 +1,10 @@
 import { TReadListProduct, TReadProductById } from "../../../application/types/product/product.repository";
 import { Product } from "../../entities/product/product.entity";
 
-export interface ProductRepositoryInterface {
-    insert(product: Product): Promise<void>;
+interface ProductRepositoryInterface {
+    insert(product: Product): Promise<Product | void>;
     findAll(): Promise<TReadListProduct>;
     findById(productId: string): Promise<TReadProductById | null>
 };
+
+export default ProductRepositoryInterface;
